8a4992d2fdae2958f05b615ad79ba9f4bf253773
Mirko Vitale <mirko.vitale.1997@outlook.com>
2024-06-12 20:29:16 +0200

    indentazione

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,14 +1,17 @@
 export function MouseClicker() {
-    function handleButtonClick(event) {
-        console.log(event.target);
-    }
+  function handleButtonClick(event) {
+    console.log(event.target);
+  }
 
-    return (
-        <div>
-        <button name="one" onClick={handleButtonClick}>Click me!</button>
-        <button name="two" onClick={handleButtonClick}>
-            <img width={24} height={24} />
-            Click me!</button>
-        </div>
-    )
+  return (
+    <div>
+      <button name="one" onClick={handleButtonClick}>
+        Click me!
+      </button>
+      <button name="two" onClick={handleButtonClick}>
+        <img width={24} height={24} />
+        Click me!
+      </button>
+    </div>
+  );
 }
